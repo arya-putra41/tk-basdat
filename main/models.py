82bddb6183a80e997d13b4a3ff84c15fe7d7d869
arya.putra41 <aryakesit4@gmail.com>
2026-04-27 20:38:05 +0700

    Create navbar, user model, login pages

--- main/models.py
+++ main/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser    
+from django.contrib.auth.base_user import BaseUserManager
 
 # Create your models here.
+
+# Custom user model needed (enforce uniqueness of email)
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email is required")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractUser):
+    username = None  # remove username field
+
+    ROLE_CHOICES = (
+        ('member', 'Member'),
+        ('staff', 'Staff'),
+    )
+    SALUTATION_CHOICES = (
+        ('Mr.', 'Mr.'),
+        ('Ms.', 'Ms.'),
+        ('Mrs.', 'Mrs.'),
+        ('Dr.', 'Dr.')
+    )
+    COUNTRY_CODE = (
+        ('+62', '+62'),
+        ('+60', '+60'),
+        ('+65', '+65')
+    )
+    COUNTRY_CHOICES = (
+        ('Indonesia', 'ID - Indonesia'),
+        ('Malaysia', 'MY - Malaysia'),
+        ('Singapura', 'SG - Singapura')
+    )
+
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    salutation = models.CharField(max_length=10, choices=SALUTATION_CHOICES)
+    first_mid_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    country_code = models.CharField(max_length=5, choices=COUNTRY_CODE)
+    mobile_number = models.CharField(max_length=20)
+    tanggal_lahir = models.DateField()
+    kewarganegaraan = models.CharField(max_length=50, choices=COUNTRY_CHOICES)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []  # since email is used instead of username
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
--- main/views.py
+++ main/views.py
@@ -1,6 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 
 def show_main(request):
     return render(request, 'main.html')
+
+# Registrasi user
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form': form}
+    return render(request, 'register.html', context)
+
+# Login user
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('main:show_main')
+
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
+
+# Log out user (no page)
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:show_main'))
+    response.delete_cookie('last_login')
+    return response
